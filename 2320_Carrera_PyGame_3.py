0938cce21efb50dcf19807fd45558c1da7e283c2
unknown <gemabravosnchez@yahoo.es>
2019-05-08 13:23:26 +0200

    v3_13_23

--- 2320_Carrera_PyGame_3.py
+++ 2320_Carrera_PyGame_3.py
@@ -4,13 +4,14 @@ import random
 
 class Runner():
     
-    # maximum number of participants = 6
-    images = ('images\001-fish.png', '005-moray.png', '003-octopus.png','006-smallball.png', '002-turtle.png', '004-prawn.png')
-    participants = ('fish', 'moray', 'octopus', 'smallball', 'turtle', 'prawn')
-    
     def __init__(self, nb_participants, startline, height):
         
         self.__nb_participants = nb_participants
+        
+        # maximum number of participants = 6
+        images = ('images/001-fish.png', 'images/005-moray.png', 'images/003-octopus.png',
+                  'images/006-smallball.png', 'images/002-turtle.png', 'images/004-prawn.png')
+        participants = ('fish', 'moray', 'octopus', 'smallball', 'turtle', 'prawn')
         
         x_coordinate_participant = startline
         y_coordinate_participant = (0.15 * height, 0.30 * height, 0.45 * height, 0.60 * height, 0.75 * height, 0.90 * height)
@@ -28,9 +29,10 @@ class Runner():
 
 class Game():
     
-    listofrunners = []
-    
     def __init__(self, nb_participants):
+        
+        self.__nb_participants = nb_participants
+        listofrunners = []
         
         # 1.- screen:
         
@@ -49,7 +51,7 @@ class Game():
         
         # 2.- participants:
         # class Runner() ---> create + customize
-        runner = Runner(nb_participants, startline = self.__startline, height = height)
+        runner = Runner(self.__nb_participants, startline = self.__startline, height = height)
         # refill listofparticipants
         listofrunners.append(runner)
     
@@ -72,15 +74,7 @@ class Game():
                 # b) runners
             for runner in listofrunners:
                 self.__screen.blit(self.runner.__custome, self.runner.__coordinates_participant)
-            
-            
-            
-            
-            
-                runner.move()
-                
-                if x_coordinate_participants >= 250:
-                    winner = True
+     
             # refresh
             pygame.display.flip()
 
@@ -88,8 +82,7 @@ class Game():
 if __name__ == '__main__':
     
     pygame.init()
-    race = Game()
-    race.compete()
+    race = Game(4)
         
         
 # blit ()        
